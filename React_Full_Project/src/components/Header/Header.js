7a15f9514dd257c527b8eff8983d1d9effc83e03
bienphamxuan <svtn-bienphamxuan@vccorp.vn>
2018-06-25 14:55:10 +0700

    first commit

---
+++ React_Full_Project/src/components/Header/Header.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
+
+class Header extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle(e) {
+    e.preventDefault();
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  sidebarToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sidebar-hidden');
+  }
+
+  sidebarMinimize(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sidebar-minimized');
+  }
+
+  mobileSidebarToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sidebar-mobile-show');
+  }
+
+  asideToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('aside-menu-hidden');
+  }
+
+  render() {
+    return (
+      <header className="app-header navbar">
+        <button className="navbar-toggler mobile-sidebar-toggler d-lg-none" onClick={this.mobileSidebarToggle} type="button">&#9776;</button>
+        <a className="navbar-brand" href="#"></a>
+        <ul className="nav navbar-nav d-md-down-none">
+          <li className="nav-item">
+            <a className="nav-link navbar-toggler sidebar-toggler" onClick={this.sidebarToggle} href="#">&#9776;</a>
+          </li>
+        </ul>
+        <ul className="nav navbar-nav ml-auto">
+          <li className="nav-item">
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+              <a onClick={this.toggle} className="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="" role="button" aria-haspopup="true" aria-expanded={this.state.dropdownOpen}>
+                <img src={'img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com"/>
+                <span className="d-md-down-none">admin</span>
+              </a>
+
+              <DropdownMenu className="dropdown-menu-right">
+                <DropdownItem header className="text-center"><strong>Account</strong></DropdownItem>
+
+                <DropdownItem><i className="fa fa-bell-o"></i> Updates<span className="badge badge-info">42</span></DropdownItem>
+                <DropdownItem><i className="fa fa-envelope-o"></i> Messages<span className="badge badge-success">42</span></DropdownItem>
+                <DropdownItem><i className="fa fa-tasks"></i> Tasks<span className="badge badge-danger">42</span></DropdownItem>
+                <DropdownItem><i className="fa fa-comments"></i> Comments<span className="badge badge-warning">42</span></DropdownItem>
+
+                <DropdownItem header className="text-center"><strong>Settings</strong></DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem><i className="fa fa-shield"></i> Lock Account</DropdownItem>
+                <DropdownItem><i className="fa fa-lock"></i> Logout</DropdownItem>
+
+              </DropdownMenu>
+            </Dropdown>
+          </li>
+          <li className="nav-item d-md-down-none">
+            <a className="nav-link navbar-toggler aside-menu-toggler" onClick={this.asideToggle} href="#">&#9776;</a>
+          </li>
+        </ul>
+      </header>
+    )
+  }
+}
+
+export default Header;
